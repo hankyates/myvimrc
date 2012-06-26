7908e639a33851aafc0e77008befd05848c9671b
Hank Yates <hankyates@gmail.com>
2012-06-26 23:30:54 +0000

    adding isntall file and doc

--- install.sh
+++ install.sh
@@ -1,13 +1,4 @@
-myvimrc
-=======
+#!/bin/bash
 
-It's my vimrc
-
-## Installation:
-
-```
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
-```
-
-or run `install.sh`
